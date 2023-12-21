eac71bf2e6d19106df61a216ca968821087789bf
Kawsar Ahmed <prokawsar@gmail.com>
2023-12-21 17:48:11 +0600

    homepage title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,10 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>Kinde Auth | SvelteKit Starter Kit</title>
+</svelte:head>
+
 <Header isAuthenticated={data.isAuthentication} user={data.userProfile} />
 
 <main>
