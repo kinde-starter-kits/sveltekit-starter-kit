73b82463bff2d8fdc7ac4e4ed854f5572735d9d8
Kawsar Ahmed <prokawsar@gmail.com>
2023-12-21 18:14:40 +0600

    format fix

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,14 +1,11 @@
-
 <footer class="footer">
-    <div class="container">
-      <strong class="text-heading-2">KindeAuth</strong>
-      <p class="footer-tagline text-body-3">
-        Visit our{" "}
-        <a href="https://kinde.com/docs">help center</a>
-      </p>
+	<div class="container">
+		<strong class="text-heading-2">KindeAuth</strong>
+		<p class="footer-tagline text-body-3">
+			Visit our{' '}
+			<a href="https://kinde.com/docs">help center</a>
+		</p>
 
-      <small class="text-subtle">
-        © 2023 KindeAuth, Inc. All rights reserved
-      </small>
-    </div>
-  </footer>
+		<small class="text-subtle"> © 2023 KindeAuth, Inc. All rights reserved </small>
+	</div>
+</footer>
